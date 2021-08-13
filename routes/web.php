99e5f8e8a2f547bf46a1338c278b31834c29349c
Rasyid Ridlo <61292802+RsydRidloo@users.noreply.github.com>
2021-08-13 20:17:43 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,11 @@ Route::get('/', function () {
   return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function() {
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
   return view('dashboard');
 })->name('dashboard');
 
-Route::group(['middleware' => 'auth'], function() {
+Route::group(['middleware' => 'auth'], function () {
   Route::resource('users', UserController::class);
   Route::resource('smkp', SMKPController::class);
-    
 });
-
