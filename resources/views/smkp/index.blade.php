56ef5372b96bac22dd34b79b01168988520c9719
dws-in <dwi.wahyu.toso@gmail.com>
2021-08-21 12:15:57 +0700

    wraptext inside smkp table

--- resources/views/smkp/index.blade.php
+++ resources/views/smkp/index.blade.php
@@ -11,7 +11,7 @@
                 <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                         <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                            <table class="divide-y divide-gray-200" width="1000">
+                            <table class="divide-y divide-gray-200 min-w-full" style="border-spacing: 0px; table-layout: fixed;">
                                 <thead>
                                 <tr>
                                     <th colspan="3" scope="col" width="50" class="px-6 py-3 bg-black text-left text-xs font-medium text-white uppercase tracking-wider">
@@ -21,29 +21,35 @@
                                 </thead>
                                 <tbody class="bg-white divide-y divide-gray-200">
                                 
-                                @php 
+                                <?php
                                     $prev_el = '';
                                     foreach ($data as $row) {
                                         if ($row->e_title == $prev_el) {
-                                            echo "<tr>
-                                                <td class=\"px-6 py-4 whitespace-nowrap text-sm text-gray-900\" style=\"word-wrap: break-word\">".$row->r_number."</td>
-                                                <td class=\"px-6 py-4 whitespace-nowrap text-sm text-gray-900\" style=\"word-wrap: break-word\">".$row->r_title."</td>
-                                            </tr>";
+                                            ?>
+                                            <tr>
+                                                <td class="px-6 py-4 text-sm text-grey-900" style="word-wrap: break-word">{{ $row->r_number }}</td>
+                                                <td class="px-6 py-4 text-sm text-grey-900" style="word-wrap: break-word">{{ $row->r_title }}</td>
+                                            </tr>
+                                            <?php
                                             $row->e_title = '';
                                         }
                                         else {
                                             if ($row->e_title != '') {
-                                                echo "<tr><td class=\"px-6 py-4 whitespace-nowrap text-sm text-white bg-gray-500\" style=\"word-wrap: break-word\">".$row->e_number."</td>
-                                                    <td class=\"px-6 py-4 whitespace-nowrap text-sm text-white bg-gray-500\" style=\"word-wrap: break-word\">".$row->e_title."</td></tr>";
+                                                ?>
+                                                <tr><td class="px-6 py-4 text-sm text-white bg-gray-500" style="word-wrap: break-word">{{ $row->e_number }}</td>
+                                                    <td class="px-6 py-4 text-sm text-white bg-gray-500" style="word-wrap: break-word">{{ $row->e_title }}</td></tr>
+                                                <?php
                                             }
                                             if ($row->r_title != NULL) {
-                                                echo "<tr><td class=\"px-6 py-4 whitespace-nowrap text-sm text-gray-900\" style=\"word-wrap: break-word\">".$row->r_number."</td>
-                                                    <td class=\"px-6 py-4 whitespace-nowrap text-sm text-gray-900\" style=\"word-wrap: break-word\">".$row->r_title."</td></tr>";
+                                                ?>
+                                                <tr><td class="px-6 py-4 text-sm text-grey-900" style="word-wrap: break-word">{{ $row->r_number }}</td>
+                                                    <td class="px-6 py-4 text-sm text-grey-900" style="word-wrap: break-word">{{ $row->r_title }}</td></tr>
+                                                <?php
                                             }
                                             $prev_el = $row->e_title;
                                         }
                                     }
-                                @endphp
+                                ?>
                                 </tbody>
                             </table>
                         </div>
