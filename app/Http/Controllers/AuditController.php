0a4bc55f75ec2f06027017600b9b4b5745bbad07
hrswcksono <hariswicaksono250999@gmail.com>
2021-09-25 17:19:56 +0700

    Update AuditController.php

--- app/Http/Controllers/AuditController.php
+++ app/Http/Controllers/AuditController.php
@@ -33,19 +33,52 @@ class AuditController extends Controller
     {
         
         $query = DB::table('requirements')->where('element_id', '=' ,$request->input('id_el'))->get();
-        ddd($request);
+
         foreach ($query as $requirement) {
+            $nilai0[] = array($requirement->n0);
+            $nilai1[] = array($requirement->n1);
+            $nilai2[] = array($requirement->n2);
+            $nilai3[] = array($requirement->n3);
+            $nilai4[] = array($requirement->n4);
+            $jawab[] = array($request->input('radio'.$requirement->id));
             $save[] = array(
-                'id' => 1,
+                'id' => Auth::user()->name,
                 'id_req'    =>   $request->input('id_req'.$requirement->id),
                 'id_el'     =>   $request->input('id_el'),
-                'jawaban'   =>  'v',
+                'jawaban'   =>  $request->input('radio'.$requirement->id),
             );
         }
-        // ddd($request);
-        
+
+        $rest = 0;
+        if($jawab == $nilai0){
+            $rest = 0;
+        } elseif($jawab == $nilai1){
+            $rest = 1;
+        } elseif($jawab == $nilai2){
+            $rest = 2;
+        } elseif($jawab == $nilai3){
+            $rest = 3;
+        } elseif($jawab == $nilai4){
+            $rest = 4;
+        }
+
+        $nilai = array(
+            'id_answer' => Auth::user()->name,
+            'id_el'     => $request->input('id_el'),
+            'nilai'     => $rest
+        );
+ 
+        $page = $request->input('id_el') + 100;
+
+        if(DB::table('requirements')->where('element_id', '=', $page)->doesntExist()){
+            $page = intval($page/10000);
+            $page = ($page+1)*10000 + 100;
+        }
+
+        DB::table('nilai')->insert($nilai);
         DB::table('answer')->insert($save);
-        return redirect('audit.create');
+
+        return redirect('audits/'.$page);
     }
 
 
