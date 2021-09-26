25f4f7f83364253bf6a3f6ae3406808cd05efb93
hrswcksono <hariswicaksono250999@gmail.com>
2021-09-26 15:25:13 +0700

    Update AuditController.php

--- app/Http/Controllers/AuditController.php
+++ app/Http/Controllers/AuditController.php
@@ -37,12 +37,12 @@ class AuditController extends Controller
         $query = DB::table('requirements')->where('element_id', '=' ,$request->input('id_el'))->get();
 
         foreach ($query as $requirement) {
-            $nilai0[] = array($requirement->n0);
-            $nilai1[] = array($requirement->n1);
-            $nilai2[] = array($requirement->n2);
-            $nilai3[] = array($requirement->n3);
-            $nilai4[] = array($requirement->n4);
-            $jawab[] = array($request->input('radio'.$requirement->id));
+            $nilai0[] = $requirement->n0;
+            $nilai1[] = $requirement->n1;
+            $nilai2[] = $requirement->n2;
+            $nilai3[] = $requirement->n3;
+            $nilai4[] = $requirement->n4;
+            $jawab[] = $request->input('radio'.$requirement->id);
             $save[] = array(
                 'id' => Auth::user()->name,
                 'id_req'    =>   $request->input('id_req'.$requirement->id),
@@ -51,43 +51,74 @@ class AuditController extends Controller
             );
         }
 
-        ddd(Auth::user()->role_id);
+        $nilai0v1 = $nilai0;$nilai0v2 = $nilai0;$nilai0v3 = $nilai0;
+        $nilai1v1 = $nilai1;$nilai1v2 = $nilai1;$nilai1v3 = $nilai1;
+        $nilai2v1 = $nilai2;$nilai2v2 = $nilai2;$nilai2v3 = $nilai2;
+        $nilai3v1 = $nilai3;$nilai3v2 = $nilai3;$nilai3v3 = $nilai3;
+        $nilai4v1 = $nilai4;$nilai4v2 = $nilai4;$nilai4v3 = $nilai4;
 
         for($i = 0; $i < count($nilai0); $i++) {
             if($nilai0[$i] == "v - x" && $nilai0[$i+1] == "v - x"){
-
+                $nilai0[$i]='v';$nilai0[$i+1]='v';$nilai0[$i+2]='x';$nilai0[$i+3]='x';
+                $nilai0v1[$i]='v';$nilai0v1[$i+1]='x';$nilai0v1[$i+2]='x';$nilai0v1[$i+3]='v';
+                $nilai0v2[$i]='x';$nilai0v2[$i+1]='x';$nilai0v2[$i+2]='v';$nilai0v2[$i+3]='v';
+                $nilai0v3[$i]='x';$nilai0v3[$i+1]='v';$nilai0v3[$i+2]='v';$nilai0v3[$i+3]='x';
             }elseif($nilai1[$i] == "v - x" && $nilai1[$i+1] == "v - x"){
-
+                $nilai1[$i]='v';$nilai1[$i+1]='v';$nilai1[$i+2]='x';$nilai1[$i+3]='x';
+                $nilai1v1[$i]='v';$nilai1v1[$i+1]='x';$nilai1v1[$i+2]='x';$nilai1v1[$i+3]='v';
+                $nilai1v2[$i]='x';$nilai1v2[$i+1]='x';$nilai1v2[$i+2]='v';$nilai1v2[$i+3]='v';
+                $nilai1v3[$i]='x';$nilai1v3[$i+1]='v';$nilai1v3[$i+2]='v';$nilai1v3[$i+3]='x';
             }elseif($nilai2[$i] == "v - x" && $nilai2[$i+1] == "v - x"){
-
+                $nilai2[$i]='v';$nilai2[$i+1]='v';$nilai2[$i+2]='x';$nilai2[$i+3]='x';
+                $nilai2v1[$i]='v';$nilai2v1[$i+1]='x';$nilai2v1[$i+2]='x';$nilai2v1[$i+3]='v';
+                $nilai2v2[$i]='x';$nilai2v2[$i+1]='x';$nilai2v2[$i+2]='v';$nilai2v2[$i+3]='v';
+                $nilai2v3[$i]='x';$nilai2v3[$i+1]='v';$nilai2v3[$i+2]='v';$nilai2v3[$i+3]='x';
             }elseif($nilai3[$i] == "v - x" && $nilai3[$i+1] == "v - x"){
-
+                $nilai3[$i]='v';$nilai3[$i+1]='v';$nilai3[$i+2]='x';$nilai3[$i+3]='x';
+                $nilai3v1[$i]='v';$nilai3v1[$i+1]='x';$nilai3v1[$i+2]='x';$nilai3v1[$i+3]='v';
+                $nilai3v2[$i]='x';$nilai3v2[$i+1]='x';$nilai3v2[$i+2]='v';$nilai3v2[$i+3]='v';
+                $nilai3v3[$i]='x';$nilai3v3[$i+1]='v';$nilai3v3[$i+2]='v';$nilai3v3[$i+3]='x';
             }elseif($nilai4[$i] == "v - x" && $nilai4[$i+1] == "v - x"){
-
-            }elseif($nilai0[$i] == "v - x"){
-
-            }elseif($nilai1[$i] == "v - x"){
-
-            }elseif($nilai2[$i] == "v - x"){
-
-            }elseif($nilai3[$i] == "v - x"){
-
-            }elseif($nilai4[$i] == "v - x"){
-
+                $nilai4[$i]='v';$nilai4[$i+1]='v';$nilai4[$i+2]='x';$nilai4[$i+3]='x';
+                $nilai4v1[$i]='v';$nilai4v1[$i+1]='x';$nilai4v1[$i+2]='x';$nilai4v1[$i+3]='v';
+                $nilai4v2[$i]='x';$nilai4v2[$i+1]='x';$nilai4v2[$i+2]='v';$nilai4v2[$i+3]='v';
+                $nilai4v3[$i]='x';$nilai4v3[$i+1]='v';$nilai4v3[$i+2]='v';$nilai4v3[$i+3]='x';
+            }elseif($nilai0[$i] == "v - x" && $nilai0[$i+1] != "v - x"){
+                $nilai0[$i]='v';$nilai0[$i+1]='x';
+                $nilai0v1[$i]='x';$nilai0v1[$i+1]='v';
+            }elseif($nilai1[$i] == "v - x" && $nilai1[$i+1] != "v - x"){
+                $nilai1[$i]='v';$nilai1[$i+1]='x';
+                $nilai1v1[$i]='x';$nilai1v1[$i+1]='v';
+            }elseif($nilai2[$i] == "v - x" && $nilai2[$i+1] != "v - x"){
+                $nilai2[$i]='v';$nilai2[$i+1]='x';
+                $nilai2v1[$i]='x';$nilai2v1[$i+1]='v';
+            }elseif($nilai3[$i] == "v - x" && $nilai3[$i+1] != "v - x"){
+                $nilai3[$i]='v';$nilai3[$i+1]='x';
+                $nilai3v1[$i]='x';$nilai3v1[$i+1]='v';
+            }elseif($nilai4[$i] == "v - x" && $nilai4[$i+1] != "v - x"){
+                $nilai4[$i]='v';$nilai4[$i+1]='x';
+                $nilai4v1[$i]='x';$nilai4v1[$i+1]='v';
             }
         }
 
         $rest = 0;
-        if($jawab == $nilai0){
-            $rest = 0;
-        } elseif($jawab == $nilai1){
-            $rest = 1;
-        } elseif($jawab == $nilai2){
-            $rest = 2;
-        } elseif($jawab == $nilai3){
-            $rest = 3;
-        } elseif($jawab == $nilai4){
-            $rest = 4;
+        for($i = (count($nilai0)-1); $i >= 0; $i--) {
+            if($jawab == $nilai0 || $jawab == $nilai0v1 || $jawab == $nilai0v2 || $jawab == $nilai0v3){
+                $rest = 0;
+                break;
+            } elseif($jawab == $nilai1 || $jawab == $nilai1v1 || $jawab == $nilai1v2 || $jawab == $nilai1v3){
+                $rest = 1;
+                break;
+            } elseif($jawab == $nilai2 || $jawab == $nilai2v1 || $jawab == $nilai2v2 || $jawab == $nilai2v3){
+                $rest = 2;
+                break;
+            } elseif($jawab == $nilai3 || $jawab == $nilai3v1 || $jawab == $nilai3v2 || $jawab == $nilai3v3){
+                $rest = 3;
+                break;
+            } elseif($jawab == $nilai4 || $jawab == $nilai4v1 || $jawab == $nilai4v2 || $jawab == $nilai4v3){
+                $rest = 4;
+                break;
+            }
         }
 
         ddd($rest);
